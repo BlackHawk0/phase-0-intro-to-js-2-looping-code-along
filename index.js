e7b3645f7d384748b2b595786c473a8e7920c0f8
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-09 19:58:55 +0300

    for loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+// returns an array of thank you messages for each name provided to the function
+
+const array = [];
+function writeCards(names, event){
+    for(let k = 0; k < names.length; k++){
+        array.push(`Thank you, ${names[k]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(num){
+    for(let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
